9f74b57839c735fd4a1546b6a5d0d3e29f509159
zameer fouzan <zfouzan@newrelic.com>
2024-11-26 19:52:58 +0530

    updated agent version; added logging

--- backendservice_firsthop/ecosystem.config.js
+++ backendservice_firsthop/ecosystem.config.js
@@ -2,6 +2,9 @@ module.exports = {
     apps : [{
       name: 'mobile_first_hop',
       script: 'server.js',
+      out_file:"logs/app.log",
+      error_file:"logs/error.log",
+      log_date_format:"YYYY-MM-DD HH:mm Z",
       env_dev: {
         NODE_ENV: "development",
         NEWRELIC_APP_NAME: "mobile_first_hop_ec2",
--- backendservice_secondhop/routes/routes.js
+++ backendservice_secondhop/routes/routes.js
@@ -7,7 +7,7 @@ const appRouter = (app, fs) => {
     // we've added in a default route here that handles empty routes
     // at the base API url
     app.get('/', (req, res) => {
-
+        console.log("second hop is working");
         res.send('<h1>Second hop is working</h1>');
     });
 
